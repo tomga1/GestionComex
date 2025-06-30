febcb7bb9f4296e7f39eab74a4cbaea0e0cc63d9
tomga1 <tomasgarcia003@gmail.com>
2025-06-30 20:16:35 -0300

    Se agregaron data anotations a cliente Model

--- GestionComex/Models/Clientes.cs
+++ GestionComex/Models/Clientes.cs
@@ -8,18 +8,20 @@ namespace GestionComex.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El CUIT es obligatorio")]
-        [StringLength(11, MinimumLength = 11, ErrorMessage = "El CUIT debe tener 11 caracteres")]
-        [RegularExpression(@"^\d{11}$", ErrorMessage = "El CUIT debe contener sólo números")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "El CUIT solo debe contener 11 números")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "El CUIT solo debe contener 11 números")]
         public string CUIT { get; set; }
 
         
         [Display(Name = "Razón Social")]
         public string RazonSocial { get; set; }  // Será de solo lectura en la vista (se puede asignar desde otro sistema)
 
-
-        [Phone(ErrorMessage = "Teléfono inválido")]
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        [RegularExpression(@"^\+?\d{7,15}$", ErrorMessage = "El teléfono debe tener solo números entre 7 y 15 digitos. Puede empezar con +")]
         public string Telefono { get; set; }
 
+
+        [Required(ErrorMessage = "La dirección es obligatoria")]
 
         [StringLength(200, ErrorMessage = "La dirección no puede superar los 200 caracteres")]
         public string? Direccion { get; set; }
